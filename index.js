07a45e81caf8c42a0a0174c1e6f4f54f4554e3dc
aneesh_augustine <aneeshaugustine911@gmail.com>
2023-04-07 11:01:58 +0530

    m

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-//document.getElementById("count-el").innerText = 5
+/* 
+let firstBatch = 5
+let srcontBatch = 7
 
-let count = 0
+let count = firstBatch + srcontBatch
 console.log(count)
+let myAge = 24
+console.log(myAge)
+
+
+let myAge = 24
+let hummendogeratio = 7
+let myDogAge = myAge * hummendogeratio */
+
+
+
+let count = 5
+
+count = count +1 
+
+document.getElementById("count-el").innerText = count 

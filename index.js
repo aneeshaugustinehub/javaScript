582226ed57f02f951ba407ee8a3849d2cf00d125
aneesh_augustine <aneeshaugustine911@gmail.com>
2023-04-08 21:28:10 +0530

    m

--- index.js
+++ index.js
@@ -1,21 +1,102 @@
+
 /* 
 let firstBatch = 5
 let srcontBatch = 7
-
 let count = firstBatch + srcontBatch
 console.log(count)
+
+
+log my age
 let myAge = 24
 console.log(myAge)
 
 
+
 let myAge = 24
 let hummendogeratio = 7
-let myDogAge = myAge * hummendogeratio */
+let myDogAge = myAge * hummendogeratio 
 
 
 
 let count = 5
-
 count = count +1 
+count = count +1 
+count = count +20
+console.log(count);
 
-document.getElementById("count-el").innerText = count 
+
+
+let bonUspoint = 50
+console.log(bonUspoint);
+bonUspoint = bonUspoint + 50
+console.log(bonUspoint);
+bonUspoint = bonUspoint - 75
+console.log(bonUspoint);
+bonUspoint = bonUspoint + 45
+console.log(bonUspoint);
+
+
+
+function countdown(){
+    console.log(5);
+    console.log(4);
+    console.log(3);
+    console.log(2);
+    console.log(1);
+}
+countdown()
+
+
+
+
+/* log 42 to colsone
+function forto(){
+    console.log(42);
+}
+forto()
+
+
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function loglapTime(){
+    let totaltime = lap1 + lap2 + lap3
+    console.log(totaltime);
+}
+loglapTime()
+
+
+// ceate a function that incriment lapcompleted when function lap is called
+let lapcompleted = 0
+
+function lap(){
+    lapcompleted = lapcompleted + 1
+
+}
+lap()
+lap()
+lap()
+
+console.log(lapcompleted);
+*/
+
+
+/*
+inizalise the count as 0
+listen ofr click on the incriment buttion
+incre ment wehen buttion clicked
+change the count-el in the html to reflect the new count
+*/
+let countel = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count = count + 1
+    countel.innerText = count
+}
+
+function save(){
+
+}
